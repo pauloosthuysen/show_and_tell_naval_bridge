19ec2f6ebd4c001dd4462d5bc39071f51f55ba57
Arno <arnom@agilebridge.co.za>
2022-07-01 13:25:13 +0200

    Added home page

--- NavalBridge/MainPage.xaml.cs
+++ NavalBridge/MainPage.xaml.cs
@@ -4,28 +4,16 @@ namespace NavalBridge;
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
-    BoardViewModel model = new BoardViewModel(8);
+	
 
 	public MainPage()
 	{
 		InitializeComponent();
 	}
 
-	private void OnCounterClicked(object sender, EventArgs e)
+	private async void Navigate(object sender, EventArgs e)
 	{
-		/*count++;
-
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
-
-		SemanticScreenReader.Announce(CounterBtn.Text);
-	}*/
-
-		// Test hit functionality
-		model.SetHit('A', 5);
-	}
+        await Navigation.PushAsync(new Pages.HomePage());
+    }
 }
 
