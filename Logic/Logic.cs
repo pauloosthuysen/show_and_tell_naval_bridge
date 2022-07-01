374536f764135fb1eddfd5eb5aaf3369c5a1e02a
Paul Oosthuysen <paulo@agilebridge.co.za>
2022-07-01 12:45:14 +0200

    desk sizes

--- Logic/Logic.cs
+++ Logic/Logic.cs
@@ -1,8 +1,24 @@
 ﻿namespace Logic
 {
+    public enum Desk {
+        Desk1,
+        Desk2,
+        Desk3,
+        Desk4,
+        Desk5,
+    }
+
     public class Logic
     {
         public int[,] grid = new int[10, 10];
+        public Dictionary<Desk, int> deskSizes = new()
+        {
+            { Desk.Desk1, 2 },
+            { Desk.Desk2, 3 },
+            { Desk.Desk3, 4 },
+            { Desk.Desk4, 5 },
+            { Desk.Desk5, 6 },
+        };
 
         public bool DetermineHit(string tileID)
         {
