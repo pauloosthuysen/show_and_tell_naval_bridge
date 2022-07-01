74994bd065ee37de04ca9a600d68e37b1ff2f48c
RohannBez <rohannb@agilebridge.co.za>
2022-07-01 14:22:29 +0200

    value

--- Logic/Logic.cs
+++ Logic/Logic.cs
@@ -46,8 +46,8 @@ namespace Logic
             Random random = new Random();
             const string chars = "ABCDE"; //FGHIJKLMNOPQRSTUVWXYZ
             const string numbers = "01234"; //56789
-            var output1 = new string(Enumerable.Repeat(chars, length).Select(s => s[random.Next(s.Length)]).ToArray());
-            var output2 = new string(Enumerable.Repeat(numbers, length).Select(s => s[random.Next(s.Length)]).ToArray());
+            var output1 = new string(Enumerable.Repeat(chars, length.Value).Select(s => s[random.Next(s.Length)]).ToArray());
+            var output2 = new string(Enumerable.Repeat(numbers, length.Value).Select(s => s[random.Next(s.Length)]).ToArray());
 
             return String.Empty;//output1 + output2;
         }
