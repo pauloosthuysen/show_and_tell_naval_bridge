1e17274c039e6f36610048908ba4d99b823aeb29
RohannBez <rohannb@agilebridge.co.za>
2022-07-01 12:24:45 +0200

    empty methods

---
+++ Logic/Logic.cs
@@ -0,0 +1,27 @@
+﻿namespace Logic
+{
+    public class Logic
+    {
+        public bool DetermineHit(string tileID)
+        {
+
+
+            return true;
+        }
+
+        public void PlaceDesksAI()
+        {
+            
+        }
+
+        public void PlaceUserShip()
+        {
+
+        }
+
+        public void ResetBoards()
+        {
+
+        }
+    }
+}
